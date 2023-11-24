945b1858fb37239a12abba6da81f3b4205196335
Юрий Кузмичев <kuzmichev@it-tian.ru>
2023-11-24 18:00:18 +0500

    feat: add  manipulation for mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -35,3 +35,56 @@ function displayCube() {
   const cube = document.querySelector("#board");
   cube.append(createTile(arr));
 }
+
+document.addEventListener("touchstart", handleTouchStart, false);
+document.addEventListener("touchmove", handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+  return (
+    evt.touches || // browser API
+    evt.originalEvent.touches
+  ); // jQuery
+}
+
+function handleTouchStart(evt) {
+  const firstTouch = getTouches(evt)[0];
+  xDown = firstTouch.clientX;
+  yDown = firstTouch.clientY;
+}
+
+function handleTouchMove(evt) {
+  if (!xDown || !yDown) {
+    return;
+  }
+
+  var xUp = evt.touches[0].clientX;
+  var yUp = evt.touches[0].clientY;
+
+  var xDiff = xDown - xUp;
+  var yDiff = yDown - yUp;
+
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    /*most significant*/
+    if (xDiff < 0) {
+      moveAndMergeRight(arr);
+      displayCube();
+    } else {
+      moveAndMergeLeft(arr);
+      displayCube();
+    }
+  } else {
+    if (yDiff < 0) {
+      moveAndMergeDown(arr);
+      displayCube();
+    } else {
+      moveAndMergeUp(arr);
+      displayCube();
+    }
+  }
+  /* reset values */
+  xDown = null;
+  yDown = null;
+}
