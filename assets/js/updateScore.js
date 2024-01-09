04dbe0cf1ac38b4af4e0ad77603f323e1470ec86
Юрий Кузмичев <kuzmichev@it-tian.ru>
2024-01-09 15:00:51 +0500

    refactoring code

--- assets/js/updateScore.js
+++ assets/js/updateScore.js
@@ -1,6 +1,17 @@
-export function updateScore(sum) {
+export function updateScore(sum, isTryAgain = false) {
   const score = document.querySelector(".score .count");
   const bestScore = document.querySelector(".best-score .count");
-  score.textContent = sum + +score.textContent;
-  bestScore.textContent = sum + +bestScore.textContent;
+  const result = isTryAgain ? 0 : sum + +score.textContent;
+  score.textContent = result;
+  if (!isTryAgain) {
+    const volumeScore = +score.textContent;
+    const volumeBestScore = +bestScore.textContent;
+    if (volumeScore > volumeBestScore) {
+      localStorage.setItem("bestScore", result);
+      bestScore.textContent = result;
+    }
+  } else {
+    const bestScoreStorage = localStorage.getItem("bestScore");
+    bestScore.textContent = bestScoreStorage === null ? 0 : bestScoreStorage;
+  }
 }
