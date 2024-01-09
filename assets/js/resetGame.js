cddd0acb1b9aac5e5db77c3f83d765c074dd6302
Юрий Кузмичев <kuzmichev@it-tian.ru>
2024-01-09 16:09:26 +0500

    create resetGame function

---
+++ assets/js/resetGame.js
@@ -0,0 +1,13 @@
+import { createTile } from "./createTile.js";
+
+export function resetGame(arr, gameOver) {
+  const tiles = document.querySelectorAll(".tile");
+  const cube = document.querySelector("#board");
+
+  gameOver?.classList.remove("active");
+
+  tiles.forEach(tile => tile.remove());
+
+  cube.append(createTile(arr));
+  cube.append(createTile(arr));
+}
