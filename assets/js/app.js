59d542fe20ff28d8a24a25904cbb3217e024670b
Юрий Кузмичев <kuzmichev@it-tian.ru>
2024-01-10 17:53:54 +0500

    refactoring code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,119 +1,38 @@
 import { createTile } from "./createTile.js";
-import { displayGameOver } from "./displayGameOver.js";
-import { moveAndMergeDown } from "./moveAndMergeDown.js";
-import { moveAndMergeLeft } from "./moveAndMergeLeft.js";
-import { moveAndMergeRight } from "./moveAndMergeRight.js";
-import { moveAndMergeUp } from "./moveAndMergeUp.js";
-import { placeRandom } from "./placeRandom.js";
+import { handleInput } from "./handleInput.js";
+import { handleTouchMove } from "./handleTouchMove.js";
+import { handleTouchStart } from "./handleTouchStart.js";
 import { resetGame } from "./resetGame.js";
 import { updateScore } from "./updateScore.js";
 let arr = Array.from({ length: 4 }, () => Array(4).fill(0));
-
-displayCube();
-displayCube();
+const cube = document.querySelector("#board");
+cube.append(createTile(arr));
+cube.append(createTile(arr));
 
 updateScore(0, true);
-window.addEventListener("keydown", handleInput);
+
+window.addEventListener("keydown", e => (arr = handleInput(e, arr)));
+
 document.querySelector(".new-game").addEventListener("click", () => {
   if (confirm("Всё слетит! Оно вам надо?")) {
     arr = Array.from({ length: 4 }, () => Array(4).fill(0));
     resetGame(arr);
   }
 });
-function handleInput(e) {
-  let isMerge = false;
-  let isMove = false;
-  switch (e.code) {
-    case "ArrowRight" || "KeyD":
-      [isMerge, isMove] = moveAndMergeRight(arr);
-      break;
-    case "ArrowLeft" || "KeyA":
-      [isMerge, isMove] = moveAndMergeLeft(arr);
-      break;
-    case "ArrowDown" || "KeyS":
-      [isMerge, isMove] = moveAndMergeDown(arr);
-      break;
-    case "ArrowUp" || "KeyW":
-      [isMerge, isMove] = moveAndMergeUp(arr);
-      break;
-  }
-  if (isMove) {
-    displayCube();
-  }
-
-  const isEmpty = arr.some(row => row.includes(0));
-
-  if (!isMerge && !isEmpty) {
-    placeRandom(arr);
-  }
-
-  if (placeRandom(arr) === true) {
-    updateScore(0, true);
-    arr = Array.from({ length: 4 }, () => Array(4).fill(0));
-    displayGameOver(arr);
-  }
-}
-
-function displayCube() {
-  const cube = document.querySelector("#board");
-  cube.append(createTile(arr));
-}
 
 // touch
-document.addEventListener("touchstart", handleTouchStart, false);
-document.addEventListener("touchmove", handleTouchMove, false);
-
 let xDown = null;
 let yDown = null;
+let touchSwitch = false;
 
-function getTouches(evt) {
-  return evt.touches || evt.originalEvent.touches;
-}
+document.addEventListener("touchstart", e => {
+  [xDown, yDown] = handleTouchStart(e, xDown, yDown);
+  touchSwitch = true;
+});
 
-function handleTouchStart(evt) {
-  const firstTouch = getTouches(evt)[0];
-  xDown = firstTouch.clientX;
-  yDown = firstTouch.clientY;
-}
-
-function handleTouchMove(evt) {
-  if (!xDown || !yDown) {
-    return;
+document.addEventListener("touchmove", e => {
+  if (touchSwitch) {
+    arr = handleTouchMove(e, arr, xDown, yDown);
+    touchSwitch = false;
   }
-
-  const xUp = evt.touches[0].clientX;
-  const yUp = evt.touches[0].clientY;
-
-  const xDiff = xDown - xUp;
-  const yDiff = yDown - yUp;
-
-  let isMerge = false;
-  let isMove = false;
-  if (Math.abs(xDiff) > Math.abs(yDiff)) {
-    /*most significant*/
-    if (xDiff < 0) {
-      [isMerge, isMove] = moveAndMergeRight(arr);
-    } else {
-      [isMerge, isMove] = moveAndMergeLeft(arr);
-    }
-  } else {
-    if (yDiff < 0) {
-      [isMerge, isMove] = moveAndMergeDown(arr);
-    } else {
-      [isMerge, isMove] = moveAndMergeUp(arr);
-    }
-  }
-  if (isMove) {
-    displayCube();
-  }
-
-  const isEmpty = arr.some(row => row.includes(0));
-
-  if (!isMerge && !isEmpty) {
-    placeRandom(arr);
-  }
-
-  /* reset values */
-  xDown = null;
-  yDown = null;
-}
+});
